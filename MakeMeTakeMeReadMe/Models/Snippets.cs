c0ce2a3cde6c1127bbbad7a1aaf39cfd9925fc9d
Tristan Emmerson <tristan@stickerslug.com>
2020-08-09 10:47:02 -0700

    create snippet model.

---
+++ MakeMeTakeMeReadMe/Models/Snippets.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+namespace MakeMeTakeMeReadMe.Models
+{
+  public class Snippets
+  {
+    private Dictionary<string, string> _snippetList = new Dictionary<string, string>();
+
+    public string GetSnippet(string key)
+    {
+      if (_snippetList.ContainsKey(key))
+      {
+        return _snippetList[key];
+      }
+      else
+      {
+        return "0";
+      }
+    }
+
+    public void AddSnippet(string key, string text)
+    {
+      _snippetList.Add(key, text);
+    }
+
+  }
+}
