645dc4eafc0efd8477648de2387b363f19b28dc6
Tristan Emmerson <tristan@stickerslug.com>
2020-08-09 11:03:56 -0700

    add text input variable references for user input. covers title/tagline/about/keywords.

--- MakeMeTakeMeReadMe/Controllers/HomeController.cs
+++ MakeMeTakeMeReadMe/Controllers/HomeController.cs
@@ -16,10 +16,18 @@ namespace MakeMeTakeMeReadMe.Controllers
     public ActionResult ReadmeTwoInput() { return View(); }
 
     [Route("/readmeOneOutput")]
-    public ActionResult ReadmeOneOutput(string text1)
+    public ActionResult ReadmeOneOutput(string title, string tagline, string about, string keyword1, string keyword2, string keyword3, string keyword4, string keyword5, string keyword6)
     {
       Snippets readmeText = new Snippets();
-      readmeText.AddText("text1", text1);
+      readmeText.AddText("title", title);
+      readmeText.AddText("tagline", tagline);
+      readmeText.AddText("about", about);
+      readmeText.AddText("keyword1", keyword1);
+      readmeText.AddText("keyword2", keyword2);
+      readmeText.AddText("keyword3", keyword3);
+      readmeText.AddText("keyword4", keyword4);
+      readmeText.AddText("keyword5", keyword5);
+      readmeText.AddText("keyword6", keyword6);
       return View(readmeText);
     }
 
